2df99bdc4eebe13e9b9d8f23a83c8f23dd57b5e1
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-24 11:46:01 +0530

    Create Question3.cpp

---
+++ Question3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+int a,b,c,d,e,f,g;
+cout<<"enter 2 no:";
+cin>>a;
+cin>>b;
+c = a+b;
+d= a-b;
+e = a*b;
+f = a/b;
+g=a%b;
+cout<<"\nAddition="<<c;
+cout<<"\nDifference="<<d;
+cout<<"\nProduct="<<e;
+cout<<"\nqotient="<<f;
+cout<<"\nRemainder="<<g;
+}
