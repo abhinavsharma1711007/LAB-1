a991d32b8fff09faeb4b41c58ca0ab58fb6802df
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-24 11:34:25 +0530

    Create Question1.cpp
    Abhinav Sharma
    Roll no. 1711007

---
+++ Question1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main(){
+int i;
+float f;
+bool b;
+cout<<"Enter integer  ";
+cin>>i;
+cout<<"\nEnter decimal  ";
+cin>>f;
+cout<<"\nEnter truth value  ";
+cin>>b;
+cout<<"\nInteger= "<<i;
+cout<<"\nDecimal Number= "<<f;
+cout<<"\nTruth Value= "<<b;
+}
