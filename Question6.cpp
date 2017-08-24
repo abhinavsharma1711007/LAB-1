0f3500bfaa3e28516bd88c6167afb91e56574fcf
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-24 11:53:20 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+float r;
+cout <<"enter radius=";
+cin >> r;
+cout<<"\n diameter= " << 2*r;
+cout<<"\n circumference= " << 2*3.14*r;
+cout<<"\n area= " << 3.14*r*r;
+}
